5cfa44ca021fb9e668779f0956429f8b0e5108d1
Liza Floyd <lizafloyd@gmail.com>
2016-09-20 14:52:29 -0400

    working prototype

--- main.js
+++ main.js
@@ -1,29 +1,77 @@
-
-// $('#pink').on("click", toggle)
-//
-// function toggle() {
-//   $(this).removeAttr("id")
-//   console.log(this)
-// }
-
 var compArray = []
 var userArray = []
-$('#play').on('click', play)
+var level = 0
 
-function play() {
+function flip(id, delay){
+  setTimeout(function(){
+        $('#' + id).addClass('active')
+    }, 1000*delay)
+    setTimeout(function(){
+        $('#' + id).removeClass('active')
+    }, 500+1000*delay)
+  }
 
-  var colors = ['pink', 'lightblue', 'yellow', 'purple', 'green']
-  var random = colors[Math.floor(Math.random() * colors.length)]
+$('.circle').on('click', pick)
 
-
-  compArray.push(random)
-  console.log(compArray)
-}
-$('.circle').on('click', select)
-// console.log(userArray)
-function select (event) {
+function pick(event) {
   event.preventDefault()
   var pick = $(this).attr('id')
   userArray.push(pick)
   console.log(userArray)
+  flip(pick, 0)
+  setTimeout(function(){
+        $(pick).addClass('active')
+    }, 1000)
+  compare()
+}
+
+$('#play').on('click', play)
+
+function play(){
+    var colors = ['pink', 'lightblue', 'yellow', 'purple', 'green']
+    var random = colors[Math.floor(Math.random() * colors.length)]
+    compArray.push(random)
+    console.log(compArray)
+    animate()
+}
+
+function compare() {
+  if (compArray.length == userArray.length) {
+  setTimeout(function(){
+    var same =  compArray.every(function(element, index){
+      return element === userArray[index]
+    })
+    if (same == true) {
+      nextLevel()
+    } else {
+      lose()
+    }
+  }, 800*level)
+}
+}
+
+function animate(){
+  for (i=0; i<compArray.length; i++){
+    flip(compArray[i],i)
+  }
+}
+
+function nextLevel(){
+  level += 1
+  $('.score').attr('value', level)
+  localStorage.setItem('value', level)
+  userArray = []
+  play()
+}
+
+function lose(){
+  document.getElementById('lose').click()
+  var score = localStorage.getItem('value')
+  console.log(score)
+  console.log('test')
+  // debugger
+  $('.score').attr('value', score)
+  // debugger
+  compArray = []
+  userArray = []
 }
