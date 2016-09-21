713bad173306bbd0dffc77e9fe6d5863c6f60c25
Liza Floyd <lizafloyd@gmail.com>
2016-09-21 10:42:17 -0400

    working again

--- main.js
+++ main.js
@@ -4,22 +4,18 @@ var level = 0
 var timeoutID
 
 function flip(id, delay){
-  setTimeout(function(){
-        $('#' + id).addClass('active')
-    }, 1000*delay)
- if (delay < 10){
-      setTimeout(function(){
+    setTimeout(function(){
+          $('#' + id).addClass('active')
+      }, 1000*delay)
+    setTimeout(function(){
           $('#' + id).removeClass('active')
       }, 500+1000*delay)
-    } else {
-      setTimeout(function(){
+    setTimeout(function(){
           $('#' + id).removeClass('active')
       }, 500+250*delay)
-    }
-  timeout()
   }
 
-$('.circle').on('click', pick)
+$('.circle').on('click',pick)
 
 function pick(event) {
   event.preventDefault()
@@ -40,6 +36,7 @@ function play(){
     var random = colors[Math.floor(Math.random() * colors.length)]
     compArray.push(random)
     animate()
+    timeout()
 }
 
 function compare() {
@@ -47,14 +44,14 @@ function compare() {
   setTimeout(function(){
     compArray.every(function(element, index){
       return element === userArray[index] }) ? nextLevel() : lose()
-  }, 800*level)
+  }, 700*level)
 }
 }
 
 function timeout(){
-  timeoutID = setTimeout(function (){
-  lose()
-}, 5000)
+    timeoutID = setTimeout(function (){
+      lose()
+    }, 3000*(level+1))
 }
 
 function animate(){
